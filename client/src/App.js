47320b4542fabf8329cffd3a09ed94ba951fde4f
Guohao Ouyang <guohoa.ouyang@gmail.com>
2020-10-09 07:32:26 -0400

    better

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css';
+<<<<<<< HEAD
 // import { Header } from './components/resume/Header';
 // import { ContactInfo } from './components/resume/ContactInfo';
 // import { BulletPointList } from './components/resume/BulletPointList';
 // import { AddTransaction } from './components/resume/AddBulletPoint';
 // import { GlobalState } from './context/GlobalState';
+=======
+>>>>>>> 8485c4d4... init commit
 import { Home } from './components/Home';
 import { Resume } from './components/resume/Resume';
 
