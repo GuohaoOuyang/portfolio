3120b5e608077806b51d4f117087a209215bad6a
Guohao Ouyang <xou96@my.yorku.ca>
2020-12-05 11:13:21 -0500

    try

--- server.js
+++ server.js
@@ -19,9 +19,10 @@ if(process.env.NODE_ENV==='development'){
 app.use('/myResume/v1', router);
 
 if(process.env.NODE_ENV==='production'){
-  app.use(express.static('client/build'));
-
-  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  const root = path.join(__dirname, 'client', 'build');
+  app.use(express.static(root));
+  app.get('*', (req,res) => res.sendFile('index.html', { root }));
+  // app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
 }
 
 const PORT = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 5000;
